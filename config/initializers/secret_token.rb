f4b53882806a5e2e81e609269e97cb6a23f5365d
Robert Szczerbak <roszczer@gmail.com>
2013-03-03 19:34:31 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depo::Application.config.secret_token = '71f945a47e3b41b35b5fd9bf7b852a2295d994d90fc87f00a841c043722911023ba45bddc266af031b653a9b167fb96d4814aa818da26430fa619e81067bb0c9'
